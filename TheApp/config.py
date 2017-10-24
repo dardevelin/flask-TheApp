b4f5ff238d5cd13a8b083ab636a143af2632a9bc
Darcy Brás da Silva <stardevelin@gmail.com>
2017-10-24 03:32:48 +0100

    Flask boilerplate config

--- TheApp/config.py
+++ TheApp/config.py
@@ -1,16 +1,12 @@
 import os
 
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/TheAppDB"
+    SQLALCHEMY_DATABASE_URI = "postgresql://username:password@localhost:port/TheAppDB"
     DEBUG = True
-    SECRET_KEY = os.environ.get("THEAPP_SECRET_KEY", os.urandom(12))
+    SECRET_KEY = os.environ.get("TheApp_SECRET_KEY", os.urandom(12))
+
 
 class TestingConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://username:password@localhost:5432/TheAppDB"
+    SQLALCHEMY_DATABASE_URI = "postgresql://username:password@localhost:port/TheAppDB"
     DEBUG = True
-    SECRET_KEY = os.environ.get("THEAPP_SECRET_KEY", os.urandom(12))
-
-class TravisConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://username:password@localhost:5432/TheAppDB"
-    DEBUG = True
-    SECRET_KEY = os.environ.get("THEAPP_SECRET_KEY", os.urandom(12))
+    SECRET_KEY = os.environ.get("TheApp_SECRET_KEY", os.urandom(12))
